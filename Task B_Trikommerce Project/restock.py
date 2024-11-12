ad00c5012c25f2edf25f948d1d5151beb0946a7a
conan-hawkins <conan.hawkins@hotmail.co.uk>
2024-11-12 20:23:44 +0000

    Improved comments on restock file

--- Task B_Trikommerce Project/restock.py
+++ Task B_Trikommerce Project/restock.py
@@ -1,10 +1,11 @@
 def restock_inventory(available_items, inventory_records, current_day):
     restock = 2000                                                           # sets 0 as the default restock number
-    if current_day == 0:
-        inventory_records += [[current_day, 0, restock, available_items]]
+    if current_day == 0:                                                     # Day 0 needs to be a restock day
+        inventory_records += [[current_day, 0, restock, available_items]]    # Adds 2000 units to restock on day 0
 
-    elif current_day % 7 == 0:                                                # if the current day is divisible by 7 and the current stock not 2000
+    elif current_day % 7 == 0:                                              # if the current day is divisible by 7 and the current stock not 2000
         restock = 2000-available_items                                      # finds amount needed for restock by subtracting the current stock by the maximum stock amount
         available_items = available_items+restock                           # availible items are the 
         inventory_records += [[current_day, 0, restock, available_items]]   # inventory records are updated
-    return available_items                                                  # return available items as integer 
+    
+    return available_items                                                  # return available items as integer
